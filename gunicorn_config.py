8afc6620279bc3cf7da2af999abc317037348bf9
naveen saini <naveensaini6378@gmail.com>
2026-06-03 20:45:37 +0530

    add gunicorn with genvt, workers

---
+++ gunicorn_config.py
@@ -0,0 +1,37 @@
+import os
+import logging
+import multiprocessing
+
+os.environ['GEVENT_SUPPORT'] = 'True'
+
+
+bind = "0.0.0.0:5000"
+backlog = 4096
+
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
+# worker_class = "gevent"
+worker_class = "geventwebsocket.gunicorn.workers.GeventWebSocketWorker"
+worker_connections = 2000
+
+timeout = 60
+graceful_timeout = 30
+keepalive = 5
+
+max_requests = 1000
+max_requests_jitter = 10
+
+preload_app = False
+
+accesslog = None
+errorlog = "-"
+loglevel = "error"
+capture_output = False
+
+access_log_format = ('%(h)s %(l)s %(u)s %(t)s ''"%(r)s" %(s)s %(b)s ''"%(f)s" "%(a)s"')
+
+limit_request_line = 8190
+limit_request_fields = 100
+limit_request_field_size = 8190
+
+reload = False
